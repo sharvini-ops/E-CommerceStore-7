04b2eedc5d13874edf8c1941818cddd114c3372e
sharvini-ops <santhsharvini7@gmail.com>
2026-05-11 11:32:48 +0530

    Login Page

--- src/main/java/com/krce/Main.java
+++ src/main/java/com/krce/Main.java
@@ -1,12 +1,29 @@
 package com.krce;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://demo.opencart.com ");
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(70));
+        driver.get("https://demo.opencart.com ");
+
+        driver.findElement(By.id("input-email")).sendKeys("santhsharvini7@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("Shar@123");
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        WebElement LoginMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("LoginMessage")));
+
+        driver.quit();
+
+    }
 }
